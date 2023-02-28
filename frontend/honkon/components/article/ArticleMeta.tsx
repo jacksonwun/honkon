9d8f202819861b81b3d0ab127db682d4b24855af
jacksonwun <jacksonwun@gmail.com>
2023-02-28 15:19:09 +0000

    update k8s

--- frontend/honkon/components/article/ArticleMeta.tsx
+++ frontend/honkon/components/article/ArticleMeta.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 import styles from "styles/home/feature/featureblog.module.scss";
 
-const ArticleMeta = ({ slug }) => {
+const ArticleMeta = ({ slug }: any) => {
   return (
     <>
       <div className="news-post-wrapper">
