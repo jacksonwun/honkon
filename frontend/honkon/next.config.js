7df8f89a10b01dbeb15bf1642ebe0ee17aec3e42
jacksonwun <jacksonwun@gmail.com>
2023-03-02 09:24:00 +0000

    update static file route

--- frontend/honkon/next.config.js
+++ frontend/honkon/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path')
+const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "_mixins.scss";`
-  }
-}
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "_mixins.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
