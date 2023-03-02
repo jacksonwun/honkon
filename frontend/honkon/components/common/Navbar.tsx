d002e2ae03ecdfde7bca38f3cfa57a6d5513d111
jacksonwun <jacksonwun@gmail.com>
2023-03-02 14:25:42 +0000

    update react_app secret

--- frontend/honkon/components/common/Navbar.tsx
+++ frontend/honkon/components/common/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navigation() {
             alt=""
             className="logo"
             width={670}
-            height={200}
+            height={60}
           />
         </Navbar.Brand>
         <Navbar.Text>
