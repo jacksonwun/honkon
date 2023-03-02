000f2a61189dc661df662ef713411d8eac610055
jacksonwun <jacksonwun@gmail.com>
2023-03-02 10:54:34 +0000

    update static file route

--- frontend/honkon/next.config.js
+++ frontend/honkon/next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "_mixins.scss";`,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "honkon-frontend.s3.eu-west-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
